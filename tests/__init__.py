5e79745cb757000b3a27b47b56aca13ee051d956
Guillem Barba <guillem@nan-tic.com>
2016-10-06 16:07:42 +0200

    Remove extra import

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,5 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from .test_analytic_account_move import suite
 try:
     from trytond.modules.analytic_account_move.tests.test_analytic_account_move import suite
 except ImportError:
