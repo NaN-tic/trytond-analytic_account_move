8dd3a2afc1e40cca4000907d3bc47ec96b9b5820
Raimon Esteve <raimon@nan-tic.com>
2017-03-27 14:41:51 +0200

    Add analytic accounts in get move line

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
+from trytond.pool import Pool
 from .account import *
 from .move import *
+from .invoice import *
 
 
 def register():
@@ -10,4 +12,5 @@ def register():
         AnalyticAccount,
         Move,
         MoveLine,
+        InvoiceLine,
         module='analytic_account_move', type_='model')
