ad26caf2c1bfeb076c2a39f207e1ea1ca31787a3
Raimon Esteve <raimon@nan-tic.com>
2017-03-29 17:25:57 +0200

    Create new analytic entry in get move line Since changeset-307147ea10ac change origin in analytic account entry, force to create new entries

--- invoice.py
+++ invoice.py
@@ -1,6 +1,6 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 
 __all__ = ['InvoiceLine']
 
@@ -10,8 +10,20 @@ class InvoiceLine:
     __name__ = 'account.invoice.line'
 
     def get_move_line(self):
+        AnalyticAccountEntry = Pool().get('analytic.account.entry')
+
         lines = super(InvoiceLine, self).get_move_line()
+
         if self.analytic_accounts:
             for line in lines:
-                line.analytic_accounts = self.analytic_accounts
+                line.analytic_lines = []
+
+                entries = []
+                for aline in self.analytic_accounts:
+                    entry = AnalyticAccountEntry()
+                    entry.root = aline.root
+                    entry.account = aline.account
+                    entries.append(entry)
+                    line.analytic_accounts = entries
+
         return lines
