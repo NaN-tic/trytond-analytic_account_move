7434d2d04e473c06ae9e3a121d2e048f8f086b88
Raimon Esteve <raimonesteve@gmail.com>
2024-09-18 12:12:15 +0200

    Post cancelled, grouped, rescheduled and delegated moves (dualrecord) From account 418d977

--- move.py
+++ move.py
@@ -1,19 +1,16 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.model import ModelView, ModelSQL, fields
+from trytond.model import ModelView, ModelSQL, dualmethod, fields
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval, If
 from trytond.transaction import Transaction
 from trytond.modules.analytic_account import AnalyticMixin
 
-__all__ = ['Move', 'MoveLine', 'AnalyticAccountEntry',
-    'MoveLineTemplate', 'AnalyticAccountLineTemplate']
-
 
 class Move(metaclass=PoolMeta):
     __name__ = 'account.move'
 
-    @classmethod
+    @dualmethod
     @ModelView.button
     def post(cls, moves):
         cls.create_analytic_lines(moves)
